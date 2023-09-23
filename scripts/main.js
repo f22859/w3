b07baf64957ac0c698cd34238f63caef60c8438e
Duff <144975304+f22859@users.noreply.github.com>
2023-09-23 15:12:08 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -20,16 +20,13 @@ let myImage = document.querySelector('img');
 
 
 myImage.onclick = () => {
-const myImage = document.querySelector("img");
-
-myImage.onclick = () => {
   const mySrc = myImage.getAttribute("src");
   if (mySrc === "images/firefox-icon.png") {
     myImage.setAttribute("src", "images/firefox2.png");
   } else {
     myImage.setAttribute("src", "images/firefox-icon.png");
   }
-}
+
 
 
 
